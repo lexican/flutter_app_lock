c556255bbdc4695c862c5d9ee3e3d30b526b9fa5
Adeyemo Adekunle <adekunle@nestcoin.com>
2023-04-14 21:00:59 +0100

    feat : Home

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_lock/features/app_lock/app_lock.dart';
+import 'package:flutter_app_lock/features/home/home.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -20,11 +21,11 @@ class _AppState extends State<App> {
             primarySwatch: Colors.blue,
           ),
           routes: <String, WidgetBuilder>{
-            '/': (context) => const SizedBox(),
+            '/': (context) => const Home(),
           },
         );
       },
-      enabled: true,
+      enabled: false,
     );
   }
 }
---
+++ lib/features/home/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Center(
+        child: Lottie.asset(
+          'assets/animations/wallet-coin.json',
+          repeat: true,
+        ),
+      ),
+    );
+  }
+}
